ef75669ebc8120fd36f242b21e0c7a14142ede14
Varun Tomar <varunrajput8077@gmail.com>
2026-02-28 18:20:52 +0530

    change the env file for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
